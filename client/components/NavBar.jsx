7660c1f36b93c0cdf48a36e2050322c6d3775a82
Kevin Dao <kevin-dao@uiowa.edu>
2022-02-19 23:18:36 -0600

    Add initial menu dropdown for navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Menu } from '@headlessui/react'
+import { MenuIcon } from '@heroicons/react/outline'
 
 export default function NavBar(props) {
   const role = props.role;
@@ -37,6 +39,22 @@ export default function NavBar(props) {
     )
   }
 
+  function SignedInButtons(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+  function NotSignedInButtons(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
   return (
     <nav className='bg-gray-800 py-2'>
       <div className='max-w-6xl mx-auto px-2'>
@@ -44,7 +62,7 @@ export default function NavBar(props) {
           {/* left */}
           <div className='flex space-x-4'>
             {/* logo */}
-            <a href="" className='text-gray-300 px-3 py-2 text-sm font-medium rounded'>Logo</a>
+            <a href="/" className='text-gray-300 px-1 py-2 text-sm font-medium rounded'>Logo</a>
 
             {/* nav links */}
             { role == 'Maintenance Company' && <MaintenanceCompanyLinks /> }
@@ -54,12 +72,36 @@ export default function NavBar(props) {
           </div>
 
           {/* right */}
-          <div className='flex space-x-4'>
+          <div className='flex space-x-4 items-center'>
             {/* profile settings */}
             <a href="" className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 text-sm font-medium rounded'>Sign Out</a>
+          
+            {/* display buttons based on log in status */}
+            <Menu as="div" className="ml-3 relative">
+              <div>
+                <Menu.Button className="">
+                  <span className='sr-only'>Open User Menu</span>
+                  <MenuIcon className='block h-6 w-6 text-gray-300 hover:text-white'/>
+                </Menu.Button>
+              </div>
+
+              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a 
+                      href="" 
+                      className={`${active && 'bg-gray-100'} block px-4 py-2 text-sm text-gray-700`}
+                    >
+                      Profile
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Menu>
           </div>
         </div>
       </div>
     </nav>
   )
 }
+
