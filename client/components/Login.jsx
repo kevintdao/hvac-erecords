5d4ba13ee943dd2d3f581a0953e84eedcb853156
Kevin Dao <kevin-dao@uiowa.edu>
2022-02-24 12:07:22 -0600

    Add empty email/pass validation

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -6,11 +6,37 @@ export default function Login(props) {
   const emailRef = useRef();
   const passwordRef = useRef();
   const [alert, setAlert] = useState({});
+  const [emailValidation, setEmailValidation] = useState("");
+  const [passwordValidation, setPasswordValidation] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    setEmailValidation("");
+    setPasswordValidation("");
+
+    // check for empty email and password
+    if(emailRef.current.value == ""){
+      setEmailValidation("Cannot be empty!");
+    }
+
+    if(passwordRef.current.value == ""){
+      setPasswordValidation("Cannot be empty!");
+    }
+
     // call server api to verify infomation
+  }
+
+  const handleEmailChange = (e) => {
+    if(e.target.value.length > 0){
+      setEmailValidation("");
+    }
+  }
+
+  const handlePassChange = (e) => {
+    if(e.target.value.length > 0){
+      setPasswordValidation("");
+    }
   }
 
   return (
@@ -22,13 +48,29 @@ export default function Login(props) {
           {/* email */}
           <div className="flex flex-col">
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" ref={emailRef} className="p-2 border border-gray-300 rounded"/>
+            <input 
+              type="text" 
+              name="email" 
+              id="email" 
+              ref={emailRef} 
+              className={`p-2 border rounded ${emailValidation ? "border-red-400" : "border-gray-300"}`}
+              onChange={handleEmailChange}
+            />
+            <span className='text-sm text-red-700 mt-1' id="email-help">{emailValidation}</span>
           </div>
 
           {/* password */}
           <div className="flex flex-col">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" ref={passwordRef} className="p-2 border border-gray-300 rounded" />
+            <input 
+              type="password" 
+              name="password" 
+              id="password" 
+              ref={passwordRef} 
+              className={`p-2 border rounded ${passwordValidation ? "border-red-400" : "border-gray-300"}`} 
+              onChange={handlePassChange}
+            />
+              <span className='text-sm text-red-700 mt-1' id="pass-help">{passwordValidation}</span>
           </div>
 
           {/* login button */}
