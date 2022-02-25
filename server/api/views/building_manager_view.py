2db7e98374ffe24dda5786bfd4b4b6a58f6ff4e0
michael-bilenko <mbilenko@uiowa.edu>
2022-02-24 23:12:23 -0600

    implementation of building manager view add/index

--- server/api/views/building_manager_view.py
+++ server/api/views/building_manager_view.py
@@ -4,3 +4,15 @@ from rest_framework.response import Response
 from base.models import BuildingManager
 from api.serializers import BuildingManagerSerializer
 
+@api_view(['GET'])
+def getData(request):
+    managers = BuildingManager.objects.all()
+    serializer = BuildingManagerSerializer(managers, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])   
+def addManager(request):
+    serializer = BuildingManagerSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
