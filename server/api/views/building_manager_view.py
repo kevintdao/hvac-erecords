06317dc1b88b9491027b2fb682fa0613b5ce4bfc
michael-bilenko <mbilenko@uiowa.edu>
2022-02-25 12:41:42 -0600

    Implement error checking

--- server/api/views/building_manager_view.py
+++ server/api/views/building_manager_view.py
@@ -4,36 +4,6 @@ from base.models import BuildingManager
 from api.serializers import BuildingManagerSerializer
 from rest_framework import status
 
-'''
-@api_view(['GET'])
-def getData(request):
-    managers = BuildingManager.objects.all()
-    serializer = BuildingManagerSerializer(managers, many=True)
-    return Response(serializer.data)
-
-@api_view(['POST'])   
-def addManager(request):
-    serializer = BuildingManagerSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(['GET'])   
-def getManager(request, pk):
-    manager = BuildingManager.objects.get(pk=pk)
-    serializer = BuildingManagerSerializer(manager, many=False)
-    return Response(serializer.data)
-
-@api_view(['POST'])   
-def updateManager(request, pk):
-    manager = BuildingManager.objects.get(pk=pk)
-    serializer = BuildingManagerSerializer(instance=manager, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-'''
-
 @api_view(['GET','POST'])   
 def apiManagers(request):
     # List managers
@@ -46,23 +16,26 @@ def apiManagers(request):
         serializer = BuildingManagerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])   
 def apiManager(request,pk):
+    try:
+        manager = BuildingManager.objects.get(pk=pk)
+    except BuildingManager.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     # Detail of manager
     if request.method == 'GET':
-        manager = BuildingManager.objects.get(pk=pk)
         serializer = BuildingManagerSerializer(manager, many=False)
         return Response(serializer.data)
     # Update manager
     elif request.method == 'PUT':
-        manager = BuildingManager.objects.get(pk=pk)
         serializer = BuildingManagerSerializer(manager, data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
-        manager = BuildingManager.objects.get(pk=pk)
         manager.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
