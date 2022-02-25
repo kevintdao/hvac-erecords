188fa821e7c6602b297a54b651f8a4d75db2b9f4
michael-bilenko <mbilenko@uiowa.edu>
2022-02-25 15:01:12 -0600

    Create unit_view.py

---
+++ server/api/views/unit_view.py
@@ -0,0 +1,5 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import BuildingManager
+from api.serializers import BuildingManagerSerializer
+from rest_framework import status
