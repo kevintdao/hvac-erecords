d2d1ffd42c2eb28481d7008be25ae92012ce4bcb
michael-bilenko <mbilenko@uiowa.edu>
2022-02-25 14:51:52 -0600

    minor spelling error

--- server/base/models/unit.py
+++ server/base/models/unit.py
@@ -5,6 +5,6 @@ class Unit(models.Model):
     category = models.CharField(max_length=255)
     serial_number = models.CharField(max_length=255)
     model_number = models.CharField(max_length=255)
-    manufactuerer = models.CharField(max_length=255)
+    manufacturer = models.CharField(max_length=255)
     production_date = models.DateField(null=True)
     installation_date = models.DateField(null=True)
--- server/base/tests/test_unit.py
+++ server/base/tests/test_unit.py
@@ -7,8 +7,8 @@ import datetime
 
 class UnitModelTests(TestCase):
     def test_created_valid_unit(self):
-        Unit.objects.create(external_id="",category="AC",serial_number="24ABC542W003102",model_number="1234A00",manufactuerer="Trane",production_date=datetime.date(2015,10,20))
-        self.assert_(Unit.objects.filter(serial_number = "24ABC542W003102", manufactuerer = 'Trane').exists())
+        Unit.objects.create(external_id="",category="AC",serial_number="24ABC542W003102",model_number="1234A00",manufacturer="Trane",production_date=datetime.date(2015,10,20))
+        self.assert_(Unit.objects.filter(serial_number = "24ABC542W003102", manufacturer = 'Trane').exists())
 
     def test_created_invalid_unit(self):
         with self.assertRaises(DataError):
