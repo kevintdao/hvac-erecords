70a7e12a3a7381dc7b72e9ea1229a7087bdad4fc
michael-bilenko <mbilenko@uiowa.edu>
2022-02-25 15:00:54 -0600

    Add units api urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('units',views.apiUnits, name = 'units-list'),
+    path('units/<int:pk>/',views.apiUnit, name = 'units-detail'),
 ]
