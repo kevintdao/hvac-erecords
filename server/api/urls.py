3c8587b5fcb6a482c6716a10cacc69c727aa3413
michael-bilenko <mbilenko@uiowa.edu>
2022-02-24 23:11:45 -0600

    building managers index and add urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('managers',views.getData, name = 'managers-list'),
+    path('managers/add/',views.addManager, name = 'managers-add'),
 ]
