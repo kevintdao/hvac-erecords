2d48fc0d6b3a84c7a5758fe4a06246524913e8f9
michael-bilenko <mbilenko@uiowa.edu>
2022-02-25 15:24:50 -0600

    allow external_id to be blank

--- server/base/models/unit.py
+++ server/base/models/unit.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Unit(models.Model):
-    external_id = models.CharField(max_length=128)
+    external_id = models.CharField(max_length=128, blank=True)
     category = models.CharField(max_length=255)
     serial_number = models.CharField(max_length=255)
     model_number = models.CharField(max_length=255)
