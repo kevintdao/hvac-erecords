7abae40de7739dc81cd29f7e03f794e663d68fd5
michael-bilenko <mbilenko@uiowa.edu>
2022-02-25 14:42:00 -0600

    Unit serializer

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from base.models import Unit
+
+class UnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Unit
+        fields = '__all__'
