e48132a9dc665cf6586d7ec11702de60214e69be
michael-bilenko <mbilenko@uiowa.edu>
2022-02-25 12:28:54 -0600

    Implement delete building manager

--- server/api/views/building_manager_view.py
+++ server/api/views/building_manager_view.py
@@ -1,8 +1,8 @@
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import BuildingManager
 from api.serializers import BuildingManagerSerializer
+from rest_framework import status
 
 '''
 @api_view(['GET'])
@@ -62,3 +62,7 @@ def apiManager(request,pk):
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        manager = BuildingManager.objects.get(pk=pk)
+        manager.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
