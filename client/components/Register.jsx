25915e53561710e4304bdecefe34d9f57bf40490
Kevin Dao <kevin-dao@uiowa.edu>
2022-02-25 14:42:24 -0600

    Add validiation check for password/confirm password

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,42 +1,39 @@
 import React, { useRef, useState } from 'react'
+import { useForm } from 'react-hook-form';
 
 export default function Register(props) {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfRef = useRef();
-  const [emailValidation, setEmailValidation] = useState("");
-  const [passwordValidation, setPasswordValidation] = useState("");
-  const [passwordConfValidation, setPasswordConfValidation] = useState("");
+  const passRef = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { ref, ...rest } = register('password', {
+    required: {
+      value: true,
+      message: "Enter a password"
+    },
+    minLength: {
+      value: 8,
+      message: "Must contains at least 8 characters"
+    },
+    pattern: {
+      value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      message: "Must contains at least 1 special character and 1 number"
+    }
+  });
 
-    // check for empty email and password
-
+  const onSubmit = (data) => {
     // call server api to verify information
   }
 
-  const handleEmailChange = (e) => {
-    if(e.target.value.length > 0){
-      setEmailValidation("");
+  const isSamePass = (passConf) => {
+    if(passRef.current.value != passConf){
+      return false;
     }
-  }
-
-  const handlePassChange = (e) => {
-    if(e.target.value.length > 0){
-      setPasswordValidation("");
-    }
-  }
-
-  const handlePassConfChange = (e) => {
-    if(e.target.value.length > 0){
-      setPasswordConfValidation("");
-    }
+    return true;
   }
 
   return (
     <div className='mt-2'>
-      <form action="" method="post" onSubmit={handleSubmit} className='flex item-center justify-center py-2'>
+      <form action="" method="post" onSubmit={handleSubmit(onSubmit)} className='flex item-center justify-center py-2'>
         <div className='w-4/5 max-w-md space-y-4 p-4 rounded-md bg-white shadow-md border border-gray-200'>
           <h2 className='text-center font-bold text-3xl'>Register</h2>
 
@@ -47,11 +44,19 @@ export default function Register(props) {
               type="text" 
               name="email" 
               id="email" 
-              ref={emailRef} 
-              className={`p-2 border rounded ${emailValidation ? "border-red-400" : "border-gray-300"}`}
-              onChange={handleEmailChange}
+              className={`p-2 border rounded ${errors.email ? "border-red-400" : "border-gray-300"}`}
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Enter an email"
+                },
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                  message: "Enter a valid email"
+                }
+              })}
             />
-            <span className='text-sm text-red-700 mt-1' id="email-help">{emailValidation}</span>
+            <span className='text-sm text-red-700 mt-1' id="email-help">{errors.email?.message}</span>
           </div>
 
           {/* password */}
@@ -61,11 +66,15 @@ export default function Register(props) {
               type="password" 
               name="password" 
               id="password" 
-              ref={passwordRef} 
-              className={`p-2 border rounded ${passwordValidation ? "border-red-400" : "border-gray-300"}`} 
-              onChange={handlePassChange}
+              ref={(e) => {
+                ref(e)
+                passRef.current = e
+              }}
+              className={`p-2 border rounded ${errors.password ? "border-red-400" : "border-gray-300"}`} 
+              {...rest}
             />
-              <span className='text-sm text-red-700 mt-1' id="pass-help">{passwordValidation}</span>
+            <span className='text-sm text-red-700 mt-1' id="pass-help">{errors.password?.message}</span>
+            <small className='text-gray-400 mt-1'>Password must contains at least 8 characters, 1 special character, and 1 number.</small>
           </div>
 
           {/* confirm password */}
@@ -75,11 +84,18 @@ export default function Register(props) {
               type="password" 
               name="password-confirm" 
               id="password-confirm" 
-              ref={passwordConfRef} 
-              className={`p-2 border rounded ${passwordConfValidation ? "border-red-400" : "border-gray-300"}`} 
-              onChange={handlePassConfChange}
+              className={`p-2 border rounded ${errors.passwordConfirm ? "border-red-400" : "border-gray-300"}`} 
+              {...register("passwordConfirm", {
+                required: {
+                  value: true,
+                  message: "Confirm your password"
+                },
+                validate: {
+                  samePass: value => isSamePass(value) || "Password does not match"
+                }
+              })}
             />
-              <span className='text-sm text-red-700 mt-1' id="pass-help">{passwordConfValidation}</span>
+            <span className='text-sm text-red-700 mt-1' id="pass-help">{errors.passwordConfirm?.message}</span>
           </div>
 
           {/* signup button */}
