989f1dea569069a37f900c9f457264d416080e5f
Andrew Murley <andrewmurley99@gmail.com>
2022-02-25 21:31:06 -0600

    Add api and technician url paths

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('technicians', views.apiTechnicians, name='technicians-list'),
+    path('technician/<int:pk>/', views.apiTechnician, name='technicians-detail')
 ]
