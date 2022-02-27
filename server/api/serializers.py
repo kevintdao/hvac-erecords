705cafe723fbf457124a84277dd32e18e4fa20cc
afilerio <alfredofilerio@gmail.com>
2022-02-27 10:01:17 -0600

    changing to new file structure

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+# from base.models import Users
+
+# class UsersSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Users
+#         fields = '__all__'
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'email', 'password', 'date_joined']
--- server/api/urls.py
+++ server/api/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
