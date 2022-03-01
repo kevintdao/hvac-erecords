a247fbcb79434c8c369d5c6ba89b317ae1390791
afilerio <alfredofilerio@gmail.com>
2022-03-01 14:24:21 -0600

    added user to building manager model

--- server/base/models/building_manager.py
+++ server/base/models/building_manager.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class BuildingManager(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=32)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
+
     
