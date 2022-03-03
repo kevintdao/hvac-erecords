59ce78ae8ed42a0fd495bfb59d6104a7f836923c
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-03 16:24:04 -0600

    Move staticfiles settings to base

--- server/server/settings/prod.py
+++ server/server/settings/prod.py
@@ -1,16 +1,5 @@
 from .base import *
-import os
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hvac-erecords-server.herokuapp.com']
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
