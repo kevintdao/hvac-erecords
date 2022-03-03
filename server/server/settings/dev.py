02579f7a838c631ee0bd1e50f5b6b2203fc4bfb7
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-03 15:48:58 -0600

    Change back database settings

--- server/server/settings/dev.py
+++ server/server/settings/dev.py
@@ -5,14 +5,3 @@ DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = ['*']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASS'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-    }
-}
