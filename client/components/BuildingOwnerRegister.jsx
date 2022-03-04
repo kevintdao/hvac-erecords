5e8bcac67ac30c8cabe13b21ef645626b7cd44d3
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-04 10:06:11 -0600

    Add email validation

--- client/components/BuildingOwnerRegister.jsx
+++ client/components/BuildingOwnerRegister.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function BuildingOwnerRegister() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    // call server api to verify information
+  }
+
   return (
     <div className='mt-2'>
-      <form action="" method="post" className='space-y-4'>
+      <form action="" method="post" onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
         <h2 className='font-bold text-3xl'>Building Owner Account Creation</h2>
 
         {/* email */}
         <div className='flex flex-col'>
           <label htmlFor="email" className="mb-2">Email</label>
-          <input type="text" name="email" id="email" className='p-2 border rounded border-gray-300' />
+          <input 
+            type="text" 
+            name="email" 
+            id="email" 
+            className={`p-2 border rounded ${errors.email ? "border-red-400" : "border-gray-300"}`} 
+            {...register('email', {
+              required: {
+                value: true,
+                message: "Enter an email"
+              },
+              pattern: {
+                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                message: "Enter a valid email"
+              },
+              maxLength: {
+                value: 320,
+                message: "Email address should not exceed 320 characters"
+              }
+            })}  
+          />
+          <span className='text-sm text-red-700 mt-1' id="email-help">{errors.email?.message}</span>
+          <small className="text-gray-400 mt-1">Email should not exceed 320 characters.</small>
         </div>
 
         <div className='grid md:grid-cols-3 md:gap-4 grid-cols-1'>
