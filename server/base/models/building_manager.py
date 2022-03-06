696bd2872bafd9ef5670ad11ab7ef00d8b3447df
afilerio <alfredofilerio@gmail.com>
2022-03-05 18:36:30 -0600

    added user

--- server/base/models/building_manager.py
+++ server/base/models/building_manager.py
@@ -4,5 +4,3 @@ class BuildingManager(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=32)
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
-
-    
