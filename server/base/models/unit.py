86283596adfb33e19c5757aa6167defc268f99af
michael-bilenko <mbilenko@uiowa.edu>
2022-03-10 16:04:04 -0600

    Update models with remaining relations

--- server/base/models/unit.py
+++ server/base/models/unit.py
@@ -2,6 +2,7 @@ from django.db import models
 from .building import *
 
 class Unit(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
     external_id = models.CharField(max_length=128, blank=True)
     category = models.CharField(max_length=255)
     serial_number = models.CharField(max_length=255)
@@ -9,4 +10,3 @@ class Unit(models.Model):
     manufacturer = models.CharField(max_length=255)
     production_date = models.DateField(null=True)
     installation_date = models.DateField(null=True)
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
