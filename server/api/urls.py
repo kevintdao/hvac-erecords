e7696e8c264b865a357c2faaf358d9fb2a928b15
afilerio <alfredofilerio@gmail.com>
2022-03-11 15:49:07 -0600

    fixed isort and flake8 linter errors in api/urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
-from . import views
+from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
@@ -13,10 +13,10 @@ urlpatterns = [
     path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
     path("user/", views.UserAPIView.as_view(), name="login"),
     path("register/", views.RegisterUserAPIView.as_view(), name="register"),
-    path("units",views.apiUnits, name = "units-list"),
-    path("units/<int:pk>/",views.apiUnit, name = "units-detail"),
-    path("managers",views.apiManagers, name = "managers-list"),
-    path("managers/<int:pk>/",views.apiManager, name = "managers-detail"),
+    path("units", views.apiUnits, name = "units-list"),
+    path("units/<int:pk>/", views.apiUnit, name = "units-detail"),
+    path("managers", views.apiManagers, name = "managers-list"),
+    path("managers/<int:pk>/", views.apiManager, name = "managers-detail"),
     path("technicians", views.apiTechnicians, name="technicians-list"),
     path("technicians/<int:pk>/", views.apiTechnician, name="technicians-detail"),
     path("buildings", views.apiBuildings, name="buildings-list"),
