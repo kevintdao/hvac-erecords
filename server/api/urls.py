658c519bc70a75287b273328c739db54df59b311
afilerio <alfredofilerio@gmail.com>
2022-03-11 15:45:13 -0600

    fixed python black linter errors in api/urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,21 +4,21 @@ from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/', views.UserAPIView.as_view(), name='login'),
-    path('register/', views.RegisterUserAPIView.as_view(), name='register'),
-    path('units',views.apiUnits, name = 'units-list'),
-    path('units/<int:pk>/',views.apiUnit, name = 'units-detail'),
-    path('managers',views.apiManagers, name = 'managers-list'),
-    path('managers/<int:pk>/',views.apiManager, name = 'managers-detail'),
-    path('technicians', views.apiTechnicians, name='technicians-list'),
-    path('technicians/<int:pk>/', views.apiTechnician, name='technicians-detail'),
-    path('buildings', views.apiBuildings, name='buildings-list'),
-    path('buildings/<int:pk>/', views.apiBuilding, name='buildings-detail'),
+    path("token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("user/", views.UserAPIView.as_view(), name="login"),
+    path("register/", views.RegisterUserAPIView.as_view(), name="register"),
+    path("units",views.apiUnits, name = "units-list"),
+    path("units/<int:pk>/",views.apiUnit, name = "units-detail"),
+    path("managers",views.apiManagers, name = "managers-list"),
+    path("managers/<int:pk>/",views.apiManager, name = "managers-detail"),
+    path("technicians", views.apiTechnicians, name="technicians-list"),
+    path("technicians/<int:pk>/", views.apiTechnician, name="technicians-detail"),
+    path("buildings", views.apiBuildings, name="buildings-list"),
+    path("buildings/<int:pk>/", views.apiBuilding, name="buildings-detail"),
 ]
