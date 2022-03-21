db56435264894eaeecbcc9cef3f0790749fbf138
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-20 21:00:49 -0500

    Add initial state management context

---
+++ client/context/state.js
@@ -0,0 +1,73 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import axios from 'axios'
+
+const AppContext = createContext()
+
+export function AppProvider ({ children }) {
+  const [data, setData] = useState({
+    accessToken: null,
+    refreshToken: null,
+    isLoggedIn: false
+  })
+
+  // remove username later
+  // hard-coded value for now since still login with username
+  function signup (inputData) {
+    const data = {
+      username: 'test',
+      email: 'test@test.com',
+      password: '123456'
+    }
+
+    axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/register`, data)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  // remove username later
+  // hard-coded value for now since still login with username
+  function login (inputData) {
+    const data = {
+      username: 'test',
+      password: '123456'
+    }
+
+    axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/token`, data)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  function logout () {
+    setData({
+      ...data,
+      accessToken: null,
+      refreshToken: null,
+      isLoggedIn: false
+    })
+  }
+
+  const value = {
+    data,
+    signup,
+    login,
+    logout
+  }
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export function useAppContext () {
+  return useContext(AppContext)
+}
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,15 @@
 import NavBar from '../components/NavBar'
 import '../styles/globals.css'
+import { AppProvider } from '../context/state'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <AppProvider>
       <NavBar role="Maintenance Company"/>
       <div className="max-w-5xl px-2 container mx-auto">
         <Component {...pageProps} />
       </div>
-    </div>
+    </AppProvider>
   )
 }
 
