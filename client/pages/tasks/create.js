c82cab37e5d8afbb7f1e1576e534399b88491795
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-23 10:02:51 -0500

    Add successfully create task screen

--- client/pages/tasks/create.js
+++ client/pages/tasks/create.js
@@ -15,9 +15,30 @@ export default function Create () {
 
   }
 
+  if (id) {
+    return (
+      <div className='mt-2'>
+        <Alert
+          title='Successful'
+          text='Successfully created a task. Click the link below to the newly created task or all the task'
+          type='success'
+        />
+
+        <div className='mt-4 space-x-4'>
+          <Link href='/tasks'>
+            <a className={styles.button}>All tasks</a>
+          </Link>
+          <Link href={`/tasks/${id}`}>
+            <a className={styles.button}>See task info</a>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className='space-y-4 mt-2'>
-      <Header title="Create Task" />
+      <Header title='Create Task' />
 
       <h2 className='font-bold text-3xl'>Create Task</h2>
 
