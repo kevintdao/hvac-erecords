3785a4ef8c1ac3fbc6e23d61a9120f7a60c742e6
michael-bilenko <mbilenko@uiowa.edu>
2022-03-23 18:26:50 -0500

    Use JSON Field to store task rules

--- server/records/models/task.py
+++ server/records/models/task.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import BaseValidator
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    # response rule field
+    rule = models.JSONField()
