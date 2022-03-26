c5eefe504e748a7e7f6e164b8bd83bdd67136f88
afilerio <alfredofilerio@gmail.com>
2022-03-26 15:21:39 -0500

    username field in user database is now just filled with email

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -15,7 +15,7 @@ class RegisterUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data['email'],
-            username=validated_data['username'],
+            username=validated_data['email'],
             password=validated_data['password']
         )
         return user
