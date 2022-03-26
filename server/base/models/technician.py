3e99e721ba0c902387b8360dce0273c1743a7f90
afilerio <alfredofilerio@gmail.com>
2022-03-26 15:46:37 -0500

    added user 1-1 relation

--- server/base/models/technician.py
+++ server/base/models/technician.py
@@ -1,7 +1,8 @@
 from django.db import models
 from .company import *
-
+from django.contrib.auth.models import User
 class Technician(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
