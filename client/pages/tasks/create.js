2a2c8f2f4b47fa7d6adb886d7e1349b93202e5f2
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-25 21:12:21 -0500

    Fix build error

--- client/pages/tasks/create.js
+++ client/pages/tasks/create.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Link from 'next/link'
 import Header from '../../components/Header'
 import TaskForm from '../../components/tasks/TaskForm'
 import Alert from '../../components/Alert'
