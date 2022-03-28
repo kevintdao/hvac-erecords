2d80e22f50458a7d87bfa9c850de7b029104dd82
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-27 23:04:01 -0500

    Add cypress test for login

--- client/cypress/integration/login.spec.js
+++ client/cypress/integration/login.spec.js
@@ -1,9 +1,46 @@
 describe('Login page', () => {
   beforeEach(() => {
+    cy.intercept('POST', '**/api/token', { fixture: 'token.json' }).as('login');
+    cy.intercept('GET', '**/api/user', [{
+      id: 1,
+      email: "test@test.com",
+      role: "Maintenance Company"
+    }]).as('getUser');
     cy.visit('http://localhost:3000/login');
   })
 
-  it('should be at the login page', () => {
-    cy.get('h2').should('contain', 'Login');
+  it('should login with the correct information', () => {
+    cy.get('input#email').type("test@test.com");
+    cy.get('input#password').type("Testing123!");
+    cy.get('button#login-button').click();
+
+    cy.wait('@login');
+    cy.wait('@getUser');
+
+    cy.url().should('include', '/');
+  })
+
+  it('should display error when login information is incorrect', () => {
+    cy.intercept('POST', '**/api/token', { statusCode: 404 }).as('loginError');
+
+    cy.get('input#email').type("test@test.com");
+    cy.get('input#password').type("Testing123@");
+    cy.get('button#login-button').click();
+
+    cy.wait('@loginError');
+
+    cy.get('#alert-title').should('contain', 'Error');
+  })
+
+  it('should save token to localStorage', () => {
+    cy.get('input#email').type("test@test.com");
+    cy.get('input#password').type("Testing123!");
+    cy.get('button#login-button').click();
+
+    cy.wait('@login');
+    cy.wait('@getUser');
+
+    cy.window().its("localStorage").invoke("getItem", "access_token").should("contain", "asdf");
+    cy.window().its("localStorage").invoke("getItem", "refresh_token").should("contain", "asdf");
   })
 })
