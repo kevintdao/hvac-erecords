e18f687b949e6ca06f5bdbecc00389f8aecd10b7
afilerio <alfredofilerio@gmail.com>
2022-03-28 21:44:57 -0500

    added many to many field for users and building manager

--- server/base/models/building_manager.py
+++ server/base/models/building_manager.py
@@ -1,7 +1,9 @@
 from django.db import models
 from .company import *
+from django.contrib.auth.models import User
 
 class BuildingManager(models.Model):
+    users = models.ManyToManyField(User)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=32)
