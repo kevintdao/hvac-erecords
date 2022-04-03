8eb2694dffd5997789c96f32f14c3094a789c3c6
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-03 11:49:01 -0500

    Add cypress tests for homepage and dashboard

--- client/cypress/integration/dashboard.spec.js
+++ client/cypress/integration/dashboard.spec.js
@@ -1,9 +1,9 @@
 describe('Home page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://localhost:3000/dashboard');
   })
 
   it('should be at the home page', () => {
-    cy.get('h2').should('contain', 'Welcome');
+    cy.get('h2').should('contain', 'Dashboard');
   })
 })
