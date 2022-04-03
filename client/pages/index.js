0e3ebea3e74061010d22e600d1298cee83eccb3b
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-03 11:44:01 -0500

    Add login and sign up button to home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 import Header from '../components/Header'
 
 export default function Home () {
+  const styles = {
+    button: 'p-2 bg-blue-700 rounded text-white text-center hover:bg-blue-800',
+  }
+
   return (
     <div className='space-y-4 mt-2'>
       <Header title='HVAC E-Records' />
 
       <h2 className='font-bold text-3xl'>Welcome to HVAC E-Records</h2>
       <p>Click the login button to login to an existing account or sign up for an account</p>
+      
+      <div className='space-x-4'>
+        <Link href='/login'><a className={styles.button}>Login</a></Link>
+        <Link href='/register'><a className={styles.button}>Sign Up</a></Link>
+      </div>
     </div>
   )
 }
