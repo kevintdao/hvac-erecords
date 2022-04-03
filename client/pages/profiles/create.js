ed632ec1765735bb816f8eddca49879e8ab855f2
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-02 21:30:55 -0500

    Update create profile page with api

--- client/pages/profiles/create.js
+++ client/pages/profiles/create.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 import axios from 'axios'
 import Header from '../../components/Header'
 import Alert from '../../components/Alert'
@@ -7,8 +8,13 @@ import Loading from '../../components/Loading'
 import { handleError } from '../../utils/errors'
 
 export default function Create () {
+  const [id, setId] = useState(null)
   const [data, setData] = useState()
   const [error, setError] = useState()
+
+  const styles = {
+    button: 'p-2 bg-indigo-700 rounded text-white text-center hover:bg-indigo-800'
+  }
 
   useEffect(() => {
     axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/tasks`)
@@ -21,27 +27,53 @@ export default function Create () {
     const tasks = formatTasks(data.tasks)
     data.tasks = tasks
 
-    // axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/profile`, data)
-    // .then(res => {
-    //   setId(res.data.id)
-    // })
-    // .catch(() => {
-      // const output = handleError(error)
-      // setError(output)
-    // })
+    console.log(data)
+
+    axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/profiles`, data)
+    .then(res => {
+      setId(res.data.id)
+    })
+    .catch(error => {
+      const output = handleError(error)
+      setError(output)
+    })
   }
 
   const formatTasks = (tasks) => {
     const numTasks = Object.keys(tasks).length
-    const output = {}
+    const output = []
     for(let i = 0; i < numTasks; i++){
-      output[i + 1] = tasks[`t${i}`]
+      output.push({
+        task_id: tasks[`t${i}`],
+        position: i + 1
+      })
     }
     return output
   }
 
   if (!data) {
     return (<Loading />)
+  }
+
+  if (id) {
+    return (
+      <div className='mt-2'>
+        <Alert
+          title='Successful'
+          text='Successfully created a profile. Click the link below to the newly created profile or all the profiles'
+          type='success'
+        />
+
+        <div className='mt-4 space-x-4'>
+          <Link href='/profiles'>
+            <a className={styles.button}>All profiles</a>
+          </Link>
+          <Link href={`/profiles/${id}`}>
+            <a className={styles.button}>See profile info</a>
+          </Link>
+        </div>
+      </div>
+    )
   }
 
   return (
