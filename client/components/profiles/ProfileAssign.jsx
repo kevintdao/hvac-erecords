85a679a7be7daf833cfbd0887112c74d0539aed5
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-06 16:51:10 -0500

    Update ProfileAssign component

--- client/components/profiles/ProfileAssign.jsx
+++ client/components/profiles/ProfileAssign.jsx
@@ -3,60 +3,83 @@ import { useForm } from 'react-hook-form'
 
 export default function ProfileAssign ({ profiles, onSubmit }) {
   const { register, handleSubmit, formState: { errors }, unregister } = useForm()
-  const [profilesList, setProfilesList] = useState([{ profile_id: profiles[0].id }])
+  const [checked, setChecked] = useState(false)
 
   const styles = {
+    inputs2Cols: 'grid md:grid-cols-2 gap-4 grid-cols-1',
     button: 'p-2 bg-blue-700 rounded text-white text-center hover:bg-blue-800',
     addButton: 'p-2 bg-green-700 rounded text-white text-center hover:bg-green-800',
     deleteButton: 'p-2 bg-red-700 rounded text-white text-center hover:bg-red-800',
     input: 'p-2 border rounded border-gray-300',
     inputContainer: 'flex flex-col',
+    checkbox: "form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
   }
 
-  const addProfile = () => {
-    setProfilesList([...profilesList, { profile_id: profiles[0].id }])
+  const onChange = (e) => {
+    setChecked(!checked)
   }
-
-  const deleteProfile = (index) => {
-    const list = [...profilesList]
-    list.splice(index, 1)
-
-    for(let i = 0; i < profilesList.length; i++){
-      unregister(`profiles.t${i}`)
-    }
-
-    setProfilesList(list)
-  }
-
-  const changeProfile = (e, index) => {
-    const list = [...profilesList]
-    list[index].profile_id = parseInt(e.target.value)
-    setProfilesList(list)
-  }
-
-  console.log(profilesList)
 
   return (
-    <form action='' method='post' onSubmit={handleSubmit(onSubmit)} className='space-y-4 mt-2'>
+    <form action="" method="post" onSubmit={handleSubmit(onSubmit)} className='space-y-2 mt-2'>
       <div className='space-y-2'>
-        <label htmlFor='profiles'>Profiles</label>
-        {profilesList.map((item, index) => (
-          <div className='flex flex-row space-x-2' key={index}>
-            <select name={`profiles.t${index}`} id={`profiles.t${index}`} value={item.profile_id} className={`${styles.input} border-gray-300 ${profilesList.length > 1 ? 'basis-5/6' : 'basis-full'}`}
-              {...register(`profiles.t${index}`, {
-                onChange: (e) => changeProfile(e, index)
+        <div className={styles.inputContainer}>
+          <label htmlFor='profiles'>Profiles</label>
+          <select name="profiles" id="profiles" className={styles.input}
+            {...register('profile')}
+          >
+            {profiles.map((item, index) => (
+              <option value={item.id} key={item.id}>{`${item.title} (${item.description})`}</option>
+            ))}
+          </select>
+        </div>
+        
+        <div className='flex flex-row space-x-4'>
+          <div>
+            <label htmlFor='is-planned'>Is Planned</label>
+            <input type="checkbox" name='is-planned' id='is-planned' className={styles.checkbox}
+              {...register('is_required', {
+                onChange: (e) => onChange(e) 
               })}
-            >
-              {profiles.map((item, index) => (
-                <option value={item.id} key={item.id}>{`${item.title} (${item.description})`}</option>
-              ))}
-            </select>
-            {profilesList.length > 1 && <button type='button' className={`${styles.deleteButton} basis-1/6`} id={`delete${index}`} onClick={() => deleteProfile(index)}>Delete</button>}
+            />
           </div>
-        ))}
-        <button type='button' className={styles.addButton} onClick={addProfile} id='add-profile'>Add Profile</button>
+          <div>
+            <label htmlFor='is-repeating'>Repeating</label>
+            <input type="checkbox" name='is-repeating' id='is-repeating' className={styles.checkbox}
+              {...register('is_repeating')}
+            />
+          </div>
+        </div>
+
+        {checked && 
+          <div className={styles.inputs2Cols}>
+            <div className={styles.inputContainer}>
+              <label htmlFor='start-date'>Start Date</label>
+              <input type="date" name='start-date' id='start-date' className={`${styles.input} ${errors.start_date ? "border-red-400" : "border-gray-300"}`}
+                {...register('start_date',{
+                  required: {
+                    value: true,
+                    message: "Enter a Start Date"
+                  }
+                })}
+              />
+              <span className='text-sm text-red-700 mt-1' id="start_date-help">{errors.start_date?.message}</span>
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor='end-date'>End Date</label>
+              <input type="date" name='end-date' id='end-date' className={`${styles.input} ${errors.end_date ? "border-red-400" : "border-gray-300"}`}
+                {...register('end_date', {
+                  required: {
+                    value: true,
+                    message: "Enter an End Date"
+                  }
+                })}
+              />
+              <span className='text-sm text-red-700 mt-1' id="end_date-help">{errors.end_date?.message}</span>
+            </div>
+          </div>
+        }
+        <button className={styles.button} id='assign-button'>Assign</button>
       </div>
-      <button className={styles.button} id='assign-button'>Assign</button>
     </form>
   )
 }
--- client/pages/units/[id].js
+++ client/pages/units/[id].js
@@ -6,16 +6,23 @@ import axios from 'axios'
 import UnitDetails from '../../components/units/UnitDetails'
 import Loading from '../../components/Loading'
 import ProfileAssign from '../../components/profiles/ProfileAssign'
+import ProfilesAssignTable from '../../components/profiles/ProfilesAssignTable'
 import { handleError } from '../../utils/errors'
 
 export default function Unit (props) {
   const router = useRouter()
   const { id } = router.query
   const [data, setData] = useState()
+  const [plans, setPlans] = useState()
   const [profiles, setProfiles] = useState()
 
   const styles = {
     button: 'p-2 bg-blue-700 rounded text-white text-center hover:bg-blue-800'
+  }
+
+  const labels = {
+    text: ['Title'],
+    id: ['title']
   }
 
   useEffect(() => {
@@ -24,7 +31,9 @@ export default function Unit (props) {
     const fetchData = async () => {
       const details = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/units/${id}/`)
       const profiles = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/profiles`)
+      const plans = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/plans`)
 
+      setPlans(plans.data)
       setProfiles(profiles.data)
       setData(details.data)
     }
@@ -33,11 +42,10 @@ export default function Unit (props) {
   }, [id, router.isReady])
 
   const onSubmit = (data) => {
-    const profiles = formatProfiles(data.profiles)
-    data.profiles = profiles
+    data.unit = id
     console.log(data)
 
-    // axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/profiles`, data)
+    // axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/plan`, data)
     // .then(res => {
     //   setId(res.data.id)
     // })
@@ -45,17 +53,6 @@ export default function Unit (props) {
     //   const output = handleError(error)
     //   setError(output)
     // })
-  }
-
-  const formatProfiles = (profiles) => {
-    const numProfiles = Object.keys(profiles).length
-    const output = []
-    for(let i = 0; i < numProfiles; i++){
-      output.push({
-        profile_id: profiles[`t${i}`],
-      })
-    }
-    return output
   }
 
   if (!data) {
@@ -86,8 +83,19 @@ export default function Unit (props) {
       <hr />
 
       <div className='space-y-2'>
-        <h2 className='font-bold text-3xl'>Assign Profile</h2>
+        <h2 className='font-bold text-3xl'>Assigned Profile</h2>
 
+        {plans.length === 0 ? 
+          <p className={styles.desc} id='no-plans'>No profiles are assigned to this unit</p> : 
+          <ProfilesAssignTable data={plans} labels={labels} />
+        }
+      </div>
+
+      <hr />
+
+      <div className='space-y-2'>
+        <h2 className='font-bold text-3xl'>Assign a Profile</h2>
+        
         <ProfileAssign profiles={profiles} onSubmit={onSubmit} />
       </div>
     </div>
