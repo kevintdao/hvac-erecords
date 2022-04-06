ad3e79c3927e0e6cdec97bfb57ec17d260135841
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-06 17:45:19 -0500

    Add ProfilesAssignTable component

--- client/pages/units/[id].js
+++ client/pages/units/[id].js
@@ -7,22 +7,26 @@ import UnitDetails from '../../components/units/UnitDetails'
 import Loading from '../../components/Loading'
 import ProfileAssign from '../../components/profiles/ProfileAssign'
 import ProfilesAssignTable from '../../components/profiles/ProfilesAssignTable'
+import Alert from '../../components/Alert'
 import { handleError } from '../../utils/errors'
 
 export default function Unit (props) {
   const router = useRouter()
   const { id } = router.query
+  const [planId, setPlanId] = useState()
+  const [error, setError] = useState()
   const [data, setData] = useState()
   const [plans, setPlans] = useState()
   const [profiles, setProfiles] = useState()
+  const [profilesList, setProfilesList] = useState()
 
   const styles = {
     button: 'p-2 bg-blue-700 rounded text-white text-center hover:bg-blue-800'
   }
 
   const labels = {
-    text: ['Title'],
-    id: ['title']
+    text: ['Title', 'Is Planned', 'Repeat', 'Start Date', 'End Date'],
+    id: ['title', 'is-required', 'is-repeat', 'start-date', 'end-date']
   }
 
   useEffect(() => {
@@ -33,7 +37,22 @@ export default function Unit (props) {
       const profiles = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/profiles`)
       const plans = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/plans`)
 
-      setPlans(plans.data)
+      let endpoints = []
+      let plansRes = plans.data
+      for (let i = 0; i < plansRes.length; i++) {
+        endpoints.push(`${process.env.NEXT_PUBLIC_HOST}/api/profiles/${plansRes[i].profile}/`)
+      }
+
+      let pList = []
+      const res = await axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+      for (let i = 0; i < res.length; i++) {
+        pList.push(res[i].data)
+      }
+
+      setPlans({
+        plan: plans.data,
+        profile: pList
+      })
       setProfiles(profiles.data)
       setData(details.data)
     }
@@ -43,20 +62,44 @@ export default function Unit (props) {
 
   const onSubmit = (data) => {
     data.unit = id
-    console.log(data)
 
-    // axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/plan`, data)
-    // .then(res => {
-    //   setId(res.data.id)
-    // })
-    // .catch(error => {
-    //   const output = handleError(error)
-    //   setError(output)
-    // })
+    // set default date when is_required is false
+    if (data.is_required == false) {
+      data.start_date = '1970-1-1'
+      data.end_date = '1970-1-1'
+    }
+
+    axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/plans`, data)
+    .then(res => {
+      setPlanId(res.data.id)
+    })
+    .catch(error => {
+      const output = handleError(error)
+      setError(output)
+    })
   }
 
   if (!data) {
     return (<Loading />)
+  }
+
+  // successfully created plan
+  if (planId) {
+    return (
+      <div className='mt-2'>
+        <Alert
+          title='Successful'
+          text='Successfully created a plan. Click the link below to view the unit/plan details'
+          type='success'
+        />
+
+        <div className='mt-4 space-x-4'>
+          <Link href={`/units/${id}`}>
+            <a className={styles.button}>Unit Details</a>
+          </Link>
+        </div>
+      </div>
+    )
   }
 
   return (
@@ -95,6 +138,8 @@ export default function Unit (props) {
 
       <div className='space-y-2'>
         <h2 className='font-bold text-3xl'>Assign a Profile</h2>
+
+        {error && <Alert title='Error' text={error} type='error' />}
         
         <ProfileAssign profiles={profiles} onSubmit={onSubmit} />
       </div>
