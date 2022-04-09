44282e562ab0f77538727741fb3853fc4e0389c4
afilerio <alfredofilerio@gmail.com>
2022-04-09 16:53:41 -0500

    removed serailizer.save

--- server/api/views/building_manager_view.py
+++ server/api/views/building_manager_view.py
@@ -22,7 +22,7 @@ def apiManagers(request):
         # )
         serializer = BuildingManagerSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            # serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
