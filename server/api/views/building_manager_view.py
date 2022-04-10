91d90dada2b546a19c47364f8c22b9f39b148997
afilerio <alfredofilerio@gmail.com>
2022-04-10 16:34:16 -0500

    updated buildingmanagerview

--- server/api/views/building_manager_view.py
+++ server/api/views/building_manager_view.py
@@ -22,8 +22,9 @@ def apiManagers(request):
         # )
         serializer = BuildingManagerSerializer(data=request.data)
         if serializer.is_valid():
-            # serializer.save()
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
