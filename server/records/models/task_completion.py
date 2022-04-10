4c4ff295fa56f7b71d7ed8fea37134d9d3354197
michael-bilenko <mbilenko@uiowa.edu>
2022-04-09 22:51:04 -0500

    Update service visit/task completion models

--- server/records/models/task_completion.py
+++ server/records/models/task_completion.py
@@ -3,13 +3,10 @@ from .task import Task
 from .service_visit import ServiceVisit
 
 class TaskCompletion(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
     task = models.ForeignKey(Task, on_delete=models.PROTECT)
-    service_visit = models.ForeignKey(ServiceVisit, on_delete=models.PROTECT, null=True)
-    # will need to do some validation
-    # -> task type is selection
-    # -> selection is valid option
-    selection = models.IntegerField(blank=True, null=True) 
-    response = models.TextField(blank=True, null=True)
-    value = models.FloatField(blank=True, null=True)
+    service_visit = models.ForeignKey(ServiceVisit, on_delete=models.PROTECT)
+    completed_at = models.DateTimeField()
+    selection = models.IntegerField(null=True, blank=True) 
+    response = models.TextField(blank=True)
+    value = models.FloatField(null=True, blank=True)
 
