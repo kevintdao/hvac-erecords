bfb47ca174404ab77ab6b46acc4f27fc1ea385e2
afilerio <alfredofilerio@gmail.com>
2022-04-10 23:13:30 -0500

    removed unnecessary files

--- server/api/views/RegisterUserAPIView.py
+++ server/api/views/RegisterUserAPIView.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import CreateAPIView
 from api.serializers import RegisterUserSerializer
 from rest_framework import permissions
-from django.core.mail import send_mail
 
 class RegisterUserAPIView(CreateAPIView):
     permission_classes = [permissions.AllowAny]
