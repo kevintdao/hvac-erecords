d8643abde6dd5809fdbb1848f940c94765c5e930
afilerio <alfredofilerio@gmail.com>
2022-04-10 22:53:44 -0500

    added send_mail to registeruserapiview

--- server/api/views/RegisterUserAPIView.py
+++ server/api/views/RegisterUserAPIView.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import CreateAPIView
 from api.serializers import RegisterUserSerializer
 from rest_framework import permissions
+from django.core.mail import send_mail
 
 class RegisterUserAPIView(CreateAPIView):
     permission_classes = [permissions.AllowAny]
