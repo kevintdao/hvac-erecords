999aefb67494ef18dca92f34b8eead9b4d1d5f2f
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-13 10:07:59 -0500

    Change svg to heroicon

--- client/pages/units/[id].js
+++ client/pages/units/[id].js
@@ -10,6 +10,7 @@ import PlanTable from '../../components/plans/PlanTable'
 import Alert from '../../components/Alert'
 import { handleError } from '../../utils/errors'
 import { QRCodeCanvas } from 'qrcode.react'
+import { DownloadIcon } from '@heroicons/react/solid'
 
 export default function Unit (props) {
   const router = useRouter()
@@ -122,9 +123,7 @@ export default function Unit (props) {
           <h2 className='font-bold text-3xl'>Unit Details</h2>
           <QRCodeCanvas id='qr-gen' value={qrValue} />
           <a onClick={downloadQRCode}>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-            </svg>
+            <DownloadIcon className='h-5 w-5 hover:cursor-pointer' />
           </a>
         </div>
 
