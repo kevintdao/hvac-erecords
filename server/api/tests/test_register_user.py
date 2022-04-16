bfa9260fcd8c32f6a4517152f00d2c7fe3f6bf14
michael-bilenko <mbilenko@uiowa.edu>
2022-04-16 18:44:32 -0500

    define test user in setup

--- server/api/tests/test_register_user.py
+++ server/api/tests/test_register_user.py
@@ -3,40 +3,39 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-class RegisterUserTest(APITestCase):
+class TestRegisterUser(APITestCase):
+    def setUp(self):
+        self.data = {
+            'email': 'test44@test.com',
+            'username': 'test44@test.com',
+            'password': 'Testing44*'
+        }
     
     def test_register_user(self):
-        data = {'email': 'test44@test.com','username': 'test44@test.com' ,'password': 'Testing44*'}
-        response = self.client.post(reverse('register'), data, format = 'json')
+        response = self.client.post(reverse('register'), self.data, format = 'json')
         
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_access_token(self):
-        data = {'email': 'test44@test.com', 'username': 'test44@test.com' ,'password': 'Testing44*'}
-        response = self.client.post(reverse('register'), data, format = 'json')
+        response = self.client.post(reverse('register'), self.data, format = 'json')
 
-        data = {'username': 'test44@test.com', 'password': 'Testing44*' }
-        response = self.client.post(reverse('token_obtain_pair'), data, format='json')
+        response = self.client.post(reverse('token_obtain_pair'), self.data, format='json')
 
         self.assertTrue('access' in response.json())
 
     def test_refresh_token(self):
-        data = {'email': 'test44@test.com','username': 'test44@test.com' ,'password': 'Testing44*'}
-        response = self.client.post(reverse('register'), data, format = 'json')
+        response = self.client.post(reverse('register'), self.data, format = 'json')
 
-        data = {'username': 'test44@test.com', 'password': 'Testing44*' }
-        response = self.client.post(reverse('token_obtain_pair'), data, format='json')
+        response = self.client.post(reverse('token_obtain_pair'), self.data, format='json')
 
         data = {'refresh': response.json()['refresh']}
         response = self.client.post(reverse('token_refresh'), data, format='json')
         self.assertTrue('access' in response.json())
 
     def test_login_user(self):
-        data = {'email': 'test44@test.com','username': 'test44@test.com' ,'password': 'Testing44*'}
-        response = self.client.post(reverse('register'), data, format = 'json')
+        response = self.client.post(reverse('register'), self.data, format = 'json')
 
-        data = {'username': 'test44@test.com', 'password': 'Testing44*' }
-        response = self.client.post(reverse('token_obtain_pair'), data, format='json')
+        response = self.client.post(reverse('token_obtain_pair'), self.data, format='json')
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.json()['access'])
 
         response = self.client.get(reverse('login'), {})
