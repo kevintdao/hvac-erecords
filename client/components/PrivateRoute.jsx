e4d8a8e0d8976feccd0f03fd5c4a16235c7c84aa
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-17 14:55:03 -0500

    Add cypress test for private route

--- client/components/PrivateRoute.jsx
+++ client/components/PrivateRoute.jsx
@@ -16,9 +16,8 @@ export default function PrivateRoute ({ isAllowed, children }) {
   }
 
   if (isAllowed && !isAllowed.includes(role)) {
-    return <div className='mt-2 font-bold text-lg'>You don&apos;t have access to this page</div>
+    return <div className='mt-2 font-bold text-lg' id='message'>You don&apos;t have access to this page</div>
   }
-
 
   return (
     {...children}
---
+++ client/cypress/integration/private-routes.spec.js
@@ -0,0 +1,17 @@
+describe('Private routes', () => {
+  it('should redirect to login page when not logged in', () => {
+    cy.visit('http://localhost:3000/dashboard');
+    
+    cy.url().should('include', '/login');
+    cy.get('#alert-title').should('contain', 'Error');
+  })
+
+  it('should display message when user doesnt have permission to access the page', () => {
+    cy.intercept('GET', '**/api/tasks', { fixture: 'all_tasks.json' }).as('getAllTasks');
+    cy.intercept('GET', '**/api/tasks/*', { fixture: 'task_numeric.json' }).as('getTasks');
+    cy.login('manager');
+    cy.visit('http://localhost:3000/tasks');
+
+    cy.get('#message').should('contain', "You don't have access to this page");
+  })
+})
