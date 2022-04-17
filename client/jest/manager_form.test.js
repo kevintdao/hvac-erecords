dc2df5b8c405a14b4f17e78035f5fab136320d63
afilerio <alfredofilerio@gmail.com>
2022-04-16 19:46:38 -0500

    added email field to tests

--- client/jest/manager_form.test.js
+++ client/jest/manager_form.test.js
@@ -4,6 +4,7 @@ import ManagerForm from "../components/managers/ManagerForm"
 const input = {
     name: "12345",
     phone_number: "(319) 123-4567",
+    email: "test@test.com"
 }
 
 afterEach(cleanup);
@@ -12,21 +13,27 @@ test('should watch input correctly', () => {
   const { container } = render(<ManagerForm type="Create"/>);
   const nameInput = container.querySelector("input#name");
   const phoneNumberInput = container.querySelector("input#phone_number");
+  const emailInput = container.querySelector("input#email");
 
   fireEvent.input(nameInput, { target: { value: input.name } });
   fireEvent.input(phoneNumberInput, { target: { value: input.phone_number } });
+  fireEvent.input(email, { target: { value: input.email } });
 
   expect(nameInput.value).toEqual(input.name);
   expect(phoneNumberInput.value).toEqual(input.phone_number);
+  expect(emailInput.value).toEqual(input.email);
+
 })
 
 test("should display error message when inputs are empty", async () => {
   const { container } = render(<ManagerForm type="Create"/>);
   const nameInput = container.querySelector("input#name");
   const phoneNumberInput = container.querySelector("input#phone_number");
+  const emailInput = container.querySelector("input#email");
 
   const nameError = container.querySelector("span#name-help");
   const phoneError = container.querySelector("span#phone-help");
+  const emailError = container.querySelector("input#email-help");
 
   const createButton = container.querySelector("button#create-button");
 
@@ -34,7 +41,9 @@ test("should display error message when inputs are empty", async () => {
 
   expect(nameInput.value.length).toEqual(0);
   expect(phoneNumberInput.value.length).toEqual(0);
+  expect(emailInput.value.length).toEqual(0);
   expect(nameError.textContent).toBe("Enter a name");
   expect(phoneError.textContent).toBe("Enter a phone number");
+  expect(emailError.textContent).toBe("Enter valid email address")
 
 })
