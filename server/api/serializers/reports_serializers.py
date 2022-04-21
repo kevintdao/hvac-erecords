3150439382f49d0a53a205accea1cab4ab2b65a8
michael-bilenko <mbilenko@uiowa.edu>
2022-04-21 00:40:36 -0500

    Add unit building to record

--- server/api/serializers/reports_serializers.py
+++ server/api/serializers/reports_serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
-from base.models import Unit
+from base.models import Unit, Building
 from records.models import Task, Profile, ProfileTask, ProfilePlan, ServiceVisit, TaskCompletion
 
 
+class BuildingDisplaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Building
+        fields = ['site_name', 'street', 'city', 'zip_code', 'country']
+
+
 class UnitRecordsSerializer(serializers.ModelSerializer):
+    building = BuildingDisplaySerializer(many=False,read_only=True)
 
     class Meta:
         model = Unit
-        fields = '__all__'
+        fields = ['id', 'external_id', 'category', 'serial_number', 
+        'model_number', 'manufacturer', 'production_date', 'installation_date',
+        'building']
