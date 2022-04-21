4823191520f3bb9e62a01c884b11d95530422ae0
michael-bilenko <mbilenko@uiowa.edu>
2022-04-21 02:32:48 -0500

    Add task completions to unit records

--- server/api/serializers/reports_serializers.py
+++ server/api/serializers/reports_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from base.models import Unit, Building
-from records.models import ServiceVisit
+from records.models import ServiceVisit, TaskCompletion
 
 
 class BuildingDisplaySerializer(serializers.ModelSerializer):
@@ -15,12 +15,29 @@ class ServiceVisitDisplaySerializer(serializers.ModelSerializer):
         model = ServiceVisit
         fields = ['id', 'technician', 'start_time', 'end_time']
 
+class TaskCompletionDisplaySerializer(serializers.ModelSerializer):
+    visit = serializers.PrimaryKeyRelatedField(source='service_visit',read_only=True)
+
+    class Meta:
+        model = TaskCompletion
+        fields = ['id','task','completed_at','selection','response','value','visit']
+
 class UnitRecordsSerializer(serializers.ModelSerializer):
     building = BuildingDisplaySerializer(many=False,read_only=True)
-    visits = ServiceVisitDisplaySerializer(many=True)
+    visits = ServiceVisitDisplaySerializer(many=True,read_only=True)
+    task_completions = serializers.SerializerMethodField()
 
     class Meta:
         model = Unit
         fields = ['id', 'external_id', 'category', 'serial_number', 
         'model_number', 'manufacturer', 'production_date', 'installation_date',
-        'building', 'visits']
+        'building', 'visits', 'task_completions']
+
+    def get_task_completions(self, unit):
+        task_completions = TaskCompletion.objects.filter(
+            service_visit__unit=unit
+        )
+        return TaskCompletionDisplaySerializer(
+            task_completions,
+            many=True
+        ).data
