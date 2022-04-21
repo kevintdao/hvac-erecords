041af516fdbc713851ed3e1429b1010c1c588b67
michael-bilenko <mbilenko@uiowa.edu>
2022-04-20 19:42:41 -0500

    Separate record serializers

---
+++ server/api/serializers/record_serializers.py
@@ -0,0 +1,92 @@
+from rest_framework import serializers
+from records.models import Task, Profile, ProfileTask, ProfilePlan, ServiceVisit, TaskCompletion
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class ProfileTaskPositionSerializer(serializers.ModelSerializer):
+    task = TaskSerializer(read_only=True)
+    task_id = serializers.PrimaryKeyRelatedField(write_only=True, source='task', queryset=Task.objects.all())
+
+    class Meta:
+        model = ProfileTask
+        fields = ('task', 'task_id', 'position')
+
+        
+class ProfileCreateSerializer(serializers.ModelSerializer):
+    tasks = ProfileTaskPositionSerializer(many=True)
+
+    class Meta:
+        model = Profile
+        fields = ('id', 'title', 'description', 'tasks')
+
+    def create(self, validated_data):
+        tasks_data = validated_data.pop('tasks')
+        profile = Profile.objects.create(**validated_data)
+        for task_data in tasks_data:
+            ProfileTask.objects.create(
+                profile=profile,
+                task=task_data.get('task'),
+                position=task_data.get('position'))
+        return profile
+
+    def update(self, instance, validated_data):
+        tasks_data = validated_data.pop('tasks')
+        # maybe should not do it this way
+        ProfileTask.objects.filter(profile=instance).delete()
+        
+        instance.title = validated_data.get('title')
+        instance.description = validated_data.get('description')
+        instance.save()
+
+        for task_data in tasks_data:
+            ProfileTask.objects.create(
+                profile=instance,
+                task=task_data.get('task'),
+                position=task_data.get('position'))
+                
+        return instance
+
+class ProfileTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfileTask
+        fields = ('task_id', 'position')
+
+class ProfileDisplaySerializer(serializers.ModelSerializer):
+    tasks = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Profile
+        fields = ('id', 'title', 'description', 'tasks')
+
+    def get_tasks(self, profile_instance):
+        query_datas = ProfileTask.objects.filter(profile=profile_instance)
+        return [ProfileTaskSerializer(task).data for task in query_datas]
+
+class ProfilePlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfilePlan
+        fields = '__all__'
+
+class ServiceVisitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceVisit
+        fields = '__all__'
+
+    def validate(self, data):
+        if data['end_time'] is not None and data['start_time'] > data['end_time']:
+            raise serializers.ValidationError("End time must occur after start time")
+        return data
+
+class TaskCompletionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskCompletion
+        fields = '__all__'
+
+    def validate(self, data):
+        service_visit = data['service_visit']
+        if (data['completed_at'] < service_visit.start_time) or (service_visit.end_time is not None and data['completed_at'] > service_visit.end_time):
+            raise serializers.ValidationError("Task completion is not in service visit start/end time range")
+        return data
