14f809de6e12d51be2c6766c7978662128d9c434
michael-bilenko <mbilenko@uiowa.edu>
2022-04-20 19:42:32 -0500

    Separate user serializers

---
+++ server/api/serializers/user_serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'date_joined']
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+        
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'date_joined']
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'last_login', 'username', 'email']
