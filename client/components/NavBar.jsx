0c2d99e65993b56dbfd140896c741fc91d7c0f22
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-29 10:18:42 -0500

    Update navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -116,9 +116,9 @@ export default function NavBar () {
     <Disclosure as="nav" className="bg-gray-800 z-50 sticky top-0">
       {({ open }) => (
         <>
-          <div className="max-w-5xl mx-auto sm:px-6 lg:px-2">
+          <div className="max-w-5xl mx-auto md:px-6 lg:px-2">
             <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -129,14 +129,14 @@ export default function NavBar () {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   {/* logo */}
                   <Link href={role ? '/dashboard' : '/'}>
                     <a className='text-gray-300 px-1 py-2 text-sm font-medium rounded'>HVAC E-Records</a>
                   </Link>
                 </div>
-                <div className="hidden sm:block sm:ml-6">
+                <div className="hidden md:block md:ml-6">
                   <div className="flex space-x-4">
                     {/* nav links */}
                     { !role && <NotLoggedInLinks /> }
@@ -147,7 +147,7 @@ export default function NavBar () {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
                 <div className='flex space-x-4 items-center'>
                   {/* display buttons based on log in status */}
                   { isLoggedIn ? <MenuDropdown /> : <NotSignedInOptions />}
@@ -156,7 +156,7 @@ export default function NavBar () {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {/* mobile nav links */}
               { !role && <NotLoggedInLinks mobile /> }
