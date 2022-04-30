6609224e47d2405a7160de88b25608b5f1c83bd7
michael-bilenko <mbilenko@uiowa.edu>
2022-04-29 23:22:20 -0500

    Add test for technician filtering

---
+++ server/api/tests/test_filtering.py
@@ -0,0 +1,47 @@
+import datetime
+
+from base.models import Company, Unit, User, Building, BuildingManager, Technician
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from rolepermissions.roles import assign_role, clear_roles
+
+
+class TestFilteringAPI(TestCase):
+    fixtures = ['test_data_filtering.json']
+
+    def setUp(self):
+        self.user_company = User.objects.get(pk=1)
+        assign_role(self.user_company, 'company')
+
+        self.user_manager = User.objects.get(pk=2)
+        assign_role(self.user_manager, 'manager')
+
+        self.company = Company.objects.get(pk=1)
+
+        self.client = APIClient()
+
+    def test_api_filter_technicians_as_company(self):
+        self.client.force_authenticate(user=self.user_company)
+        technicians = Technician.objects.filter(company=self.company)
+        response = self.client.get(reverse('technicians-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        for technician in response.data:
+            self.assert_(technicians.filter(pk=technician['user']).exists())
+        
+        technician_related = technicians.first()
+        response = self.client.get(
+            reverse('technicians-detail',
+            kwargs={'pk':technician_related.user_id}), format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        technician_not_related = Technician.objects.exclude(company=self.company).first()
+        response = self.client.get(
+            reverse('technicians-detail',
+            kwargs={'pk':technician_not_related.user_id}), format="json"
+        )
+        self.assertNotEqual(response.status_code, status.HTTP_200_OK)
+
+    
