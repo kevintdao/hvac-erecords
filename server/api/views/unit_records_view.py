bc57e4273642704b8f39aaf41d058fe32dfc1508
michael-bilenko <mbilenko@uiowa.edu>
2022-04-30 03:44:19 -0500

    Add unit filter to unit records view

--- server/api/views/unit_records_view.py
+++ server/api/views/unit_records_view.py
@@ -5,15 +5,16 @@ from base.models import Unit
 from rest_framework import status
 from api.serializers import UnitRecordsSerializer
 from rolepermissions.checkers import has_permission
+from .unit_view import filter_units
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def apiUnitRecords(request, pk):
     try:
-        unit = Unit.objects.get(pk=pk)
+        unit = filter_units(request.user).get(pk=pk)
     except Unit.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    # Detail of unit
+    # Unit records
     if request.method == 'GET' and has_permission(request.user, 'get_unit_records'):
         serializer = UnitRecordsSerializer(unit, many=False)
         return Response(serializer.data)
