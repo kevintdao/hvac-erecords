d9566f017cc41ff76a5c04390aee6b57f312f05d
afilerio <alfredofilerio@gmail.com>
2022-05-01 20:34:38 -0500

    Added user import to file and token to PasswordResetTokenGenerator

--- server/api/views/PasswordTokenCheckAPIView.py
+++ server/api/views/PasswordTokenCheckAPIView.py
@@ -4,12 +4,13 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from rest_framework import status
 from rest_framework.response import Response
 from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from base.models import User
 class PasswordTokenCheckAPI(GenericAPIView):
     def get(self, request, uidb64, token):
         try:
             user_id = smart_str(urlsafe_b64decode(uidb64))
             user=User.objects.get(id=user_id)
-            if not PasswordResetTokenGenerator().check_token(user):
+            if not PasswordResetTokenGenerator().check_token(user, token):
                 return Response({'error': 'Token is not valid'}, status=status.HTTP_401_UNAUTHORIZED)
             
             return Response({'success':True, 'message': 'Credentials Valid', 'uidb64': uidb64, 'token': token}, status=status.HTTP_200_OK)
