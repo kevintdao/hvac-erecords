475da9fd3b3dd3407e6b0b1183adc530ce2f5d0e
Kevin Dao <kevin-dao@uiowa.edu>
2022-05-01 21:28:13 -0500

    Add TechnicianHelp component

--- client/components/help/NotLoggedIn.jsx
+++ client/components/help/NotLoggedIn.jsx
@@ -14,11 +14,11 @@ export default function NotLoggedIn() {
         <h2 className={styles.header}>Login</h2>
         <p>To login to the site, enter your email and password. When you enter the incorrect information,
           an error message will display letting you that your information is incorrect.
-          As a maintenance company, you can sign up for an account by clicking on the "Don't have an account?" link or
-          clicking on the "Sign Up" button on the navigation bar.
+          As a maintenance company, you can sign up for an account by clicking on the &quot;Don&apos;t have an account?&quot; link or
+          clicking on the &quot;Sign Up&quot; button on the navigation bar.
           As a technician, building managers, or inspector, only your maintenance company can create an account for you.
         </p>
-        <Image src={loginpage} />
+        <Image src={loginpage} alt='loginPage' />
       </div>
 
       <div>
@@ -26,7 +26,7 @@ export default function NotLoggedIn() {
         <p>To sign up for a maintenance company account, enter your company informations along
           with an email and password associated with that company.
         </p>
-        <Image src={loginpage} />
+        <Image src={loginpage} alt='signUpPage' />
       </div>
     </div>
   )
