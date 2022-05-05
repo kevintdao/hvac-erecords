0e2e972cb122bd0e507754b196fe801377440125
michael-bilenko <mbilenko@uiowa.edu>
2022-05-05 00:10:51 -0500

    Add full charge to refrigerant report

--- server/api/serializers/refrigerant_report_serializers.py
+++ server/api/serializers/refrigerant_report_serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.utils.serializer_helpers import ReturnDict
 from base.models import Unit, Building, Technician
-from records.models import ServiceVisit, TaskCompletion, ProfilePlan
+from records.models import ServiceVisit, TaskCompletion, ProfilePlan, Profile
 
 class DictSerializer(serializers.ListSerializer):
     """
@@ -25,8 +25,42 @@ class DictSerializer(serializers.ListSerializer):
         return {item[self.dict_key]: item for item in items}
 
 
+
+class FullChargeSerializer(serializers.ModelSerializer):
+    time = serializers.DateTimeField(source='end_time',read_only=True)
+    value = serializers.SerializerMethodField()
+    method = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ServiceVisit
+        fields = ['time', 'value', 'method']
+
+    def get_value(self, visit):
+        ## TODO: determine if we want to assume task_id is in correct order
+        return visit.completions.all().order_by('task_id')[0].value
+
+    def get_method(self, visit):
+        return visit.completions.all().order_by('task_id')[1].response
+
+
 class RefrigerantReportSerializer(serializers.ModelSerializer):
+    operator = serializers.CharField(source='building.manager.name',read_only=True)
+    street = serializers.CharField(source='building.street',read_only=True)
+    city = serializers.CharField(source='building.city',read_only=True)
+    zip_code = serializers.CharField(source='building.zip_code',read_only=True)
+    country = serializers.CharField(source='building.country',read_only=True)
+    full_charge = serializers.SerializerMethodField()
 
     class Meta:
         model = Unit
-        fields = '__all__'
+        fields = ('id', 'serial_number', 'operator', 'street', 'city', 'zip_code', 'country', 'full_charge')
+
+    def get_full_charge(self, unit):
+        profile = Profile.objects.filter(tag="epa608-charge").first()
+        # TODO: What if plan changes/deleted
+        visits = ServiceVisit.objects.filter(unit=unit,plan__profile=profile)
+
+        return FullChargeSerializer(
+            visits,
+            many=True
+        ).data
