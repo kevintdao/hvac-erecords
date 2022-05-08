a747a8ac16035ee8d4fff02ced821318ddae1ffd
Kevin Dao <kevin-dao@uiowa.edu>
2022-05-07 22:43:21 -0500

    Fix styling

--- client/pages/password-set/[uidb64]/[token].js
+++ client/pages/password-set/[uidb64]/[token].js
@@ -3,30 +3,14 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useForm, } from 'react-hook-form';
 import Alert from '../../../components/Alert';
+import Header from '../../../components/Header'
+
 export default function ResetPassword() {
     const router = useRouter();
     const { uidb64, token } = router.query;
 
     const [error, setError] = useState()
     const [success, setSuccess] = useState(false)
-    const onSubmit = async (data) => {
-      data = {
-        "password": data.password,
-        "token": token,
-        "uidb64": uidb64
-      }
-      axios.patch(`${process.env.NEXT_PUBLIC_HOST}/api/password-set-complete/`, data)
-        .then(res => {
-            setSuccess(true)
-          
-        })
-        .catch(() => {
-          // const output = handleError(error)
-          alert('no')
-        })
-    }
-
-
 
     const { register, handleSubmit, formState: { errors }, watch } = useForm({
       mode: 'onTouched'
@@ -40,6 +24,23 @@ export default function ResetPassword() {
         helpText: "text-sm text-red-700 mt-1",
         button: "p-2 bg-blue-700 rounded text-white text-center hover:bg-blue-800",
     }
+
+    const onSubmit = async (data) => {
+      data = {
+        "password": data.password,
+        "token": token,
+        "uidb64": uidb64
+      }
+      axios.patch(`${process.env.NEXT_PUBLIC_HOST}/api/password-set-complete/`, data)
+        .then(res => {
+          setSuccess(true)
+        })
+        .catch(err => {
+          const output = handleError(error)
+          setError(output)
+        })
+    }
+
     if(success){
       return(
       <div className='mt-2'>
@@ -51,10 +52,12 @@ export default function ResetPassword() {
       </div>
       )
     }
+
     return (
-        <>
+        <div className='space-y-4 mt-2'>
+            <Header title='Set password' />
+            <h2 className="font-bold text-3xl">Set Password</h2>
             {error && <Alert title='Error' text={error} type='error' />}
-            <div className="text-4xl mt-5 mb-3">Password Set Page</div>
             <form action="" method="post" onSubmit={handleSubmit(onSubmit)} className="space-y-4 mt-4">
               <div className={styles.inputContainer}>
                 <label htmlFor='password'>Password</label>
@@ -96,7 +99,7 @@ export default function ResetPassword() {
               <input type="submit" className={styles.button} id='password-button' value="Set Password"/>
             </form>
 
-        </>
+        </div>
         
   )
 }
